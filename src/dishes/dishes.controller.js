060bdceb528b6572a7d6b041625a0d0fc7dec860
Nick Engelhardt <enickgoku2@gmail.com>
2022-08-22 12:49:09 -0400

    added dishes middleware

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -41,9 +41,86 @@ const update = (req, res) => {
   res.json({ data: dishData })
 }
 
+//middleware validation
+
+const dishExists = (req, res, next) => {
+  const { dishId } = req.params
+  const foundDish = dishes.find(dish => dish.id === Number(dishId))
+
+  if(foundDish) {
+    res.locals.dish = foundDish
+    return next()
+  }
+  next({
+    status: 404,
+    message: `Dish does not exist: ${dishId}.`
+  })
+}
+
+const dishHasName = (req, res, next) => {
+  const { data = {} } = req.body
+
+  if (!data.name) {
+    next({
+      status: 400,
+      message: "Dish must include a name."
+    })
+  }
+  res.locals.reqBody = data
+  return next()
+}
+
+const dishHasDescription = (req, res, next) => {
+  const reqBody = res.locals.reqBody
+
+  if (!reqBody) {
+    next({
+      status: 400,
+      message: "Dish must include a description."
+    })
+  }
+  return next()
+}
+
+const dishHasPrice = (req, res, next) => {
+  const reqBody = res.locals.reqBody
+
+  if (!reqBody.price || reqBody.price < 0 || typeof reqBody.price !== "number") {
+    next({
+      status: 400,
+      message:
+        "Dish must include a price and it must be an integer greater than 0.",
+    })
+  }
+  return next()
+}
+
+const dishHasImageUrl = (req, res, next) => {
+  const reqBody = res.locals.reqBody
+
+  if (!reqBody["image_url"]) {
+    next({
+      status: 400,
+      message: "Dish must include a image_url"
+    })
+  }
+  return next()
+}
+
 module.exports = {
   list,
-  create: [create],
-  read: [read],
-  update: [update]
+  create: 
+    [dishHasName,
+    dishHasDescription,
+    dishHasPrice,
+    dishHasImageUrl,
+    create],
+  read: [dishExists, read],
+  update: 
+    [dishExists,
+    dishHasName,
+    dishHasDescription,
+    dishHasPrice,
+    dishHasImageUrl,
+    update]
 }
